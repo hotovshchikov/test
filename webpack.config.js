c75fd27dce2e1f7e1f1467f1cbfa108cf8b3b66b
Zerk <axzerk@gmail.com>
2017-06-10 02:05:30 +0300

    switched babel es2015 to env

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
                     {
                         loader: 'babel-loader',
                         options: {
-                            presets: [['es2015', {modules: false}], 'react'],
+                            presets: [['env', {modules: false}], 'react'],
                             plugins: ['react-hot-loader/babel']
                         }
                     }
--- webpack.config.production.js
+++ webpack.config.production.js
@@ -27,7 +27,7 @@ module.exports = {
                     {
                         loader: 'babel-loader',
                         options: {
-                            presets: [['es2015', {modules: false}], 'react'],
+                            presets: [['env', {modules: false}], 'react'],
                         }
                     }
                 ]
