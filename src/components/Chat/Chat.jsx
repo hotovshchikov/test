b06a042639f0e82056d153ef48b9aa1037bebfa6
Zerk <axzerk@gmail.com>
2017-06-12 14:20:17 +0300

    minor fixes

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Message from './message/Message';
+import Message from './Message/Message';
 
 export default class Chat extends React.Component {
     constructor() {
